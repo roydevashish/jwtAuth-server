37a4ffc09e15c83c355141d7e42a1d30dc01e15c
Devashish Roy <medevashishroy@gmail.com>
2025-09-04 21:07:57 +0530

    fix: fix typo

--- src/utils/APIError.js
+++ src/utils/APIError.js
@@ -1,5 +1,5 @@
 class APIError extends Error {
-  constructor(statusCode, message = "Something went wrong.", error = [], stack = ""){
+  constructor(statusCode, message = "Something went wrong.", errors = [], stack = ""){
     super(message);
     this.statusCode = statusCode;
     this.data = null;
