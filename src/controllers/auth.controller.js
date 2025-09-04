5517569e55fa364781f899e0adb26e31084a5bb4
Devashish Roy <medevashishroy@gmail.com>
2025-09-04 22:21:06 +0530

    feat: create user login api

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import asyncHandler from "../utils/asyncHandler.js";
 import User from "../models/user.model.js";
 import APIResponse from "../utils/APIResponse.js";
 import APIError from "../utils/APIError.js";
+import { cookieOptions } from "../utils/cookie.js";
 
 const register = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -38,6 +39,52 @@ const register = asyncHandler(async (req, res) => {
   }
 });
 
+const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new APIError(400, "Email and Password both required.");
+  }
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if(!existingUser) {
+      throw new APIError(400, "User doesn't exists.");
+    }
+
+    const isPasswordCorrect = await existingUser.isPasswordCorrect(password);
+    if(!isPasswordCorrect) {
+      throw new APIError(400, "Invalid credentials.");
+    }
+
+    const accessToken = existingUser.generateAccessToken();
+    const refreshToken = existingUser.generateRefreshToken();
+
+    existingUser.refreshToken = refreshToken;
+    await existingUser.save();
+
+    const resData = {
+      "_id": existingUser._id,
+      "email": existingUser.email,
+      "access-token": accessToken,
+      "refresh-token": refreshToken
+    }
+
+    res.status(200)
+    .cookie("access-token", accessToken, cookieOptions)
+    .cookie("refresh-token", refreshToken, cookieOptions)
+    .json(
+      new APIResponse(200, resData, "User logged in successfully.")
+    );
+  } catch (error) {
+    console.log("🚫 Internal server error: ");
+    console.log(error);
+    res.status(500).json(
+      new APIError(500, "Internal server error.")
+    );
+  }
+});
+
 export {
-  register
+  register,
+  login
 }
--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { register } from "../controllers/auth.controller.js";
+import { register, login } from "../controllers/auth.controller.js";
 
 const router = Router();
 
 router.route("/register").post(register);
+router.route("/login").post(login);
 
 export default router;
